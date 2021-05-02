f82715f8eb3a17dcb4c6e5d20c0fcb51e291ab5b
thundermage117 <72457864+thundermage117@users.noreply.github.com>
2021-05-02 18:00:57 +0530

    Update header.h
    rearranging structs

--- header.h
+++ header.h
@@ -4,20 +4,16 @@
 #define NUM 100000
 #define DAY_MAX 15
 
+typedef struct __day _day;
+typedef struct __person _person;
+typedef struct __station _station;
+typedef struct __path _path;
+typedef struct __route _route;
+typedef struct __list _list;
+typedef enum __status _status;
+
 int cur_day;
 enum  __status {negative, secondary_contact, primary_contact, positive};
-struct __day {
-	_person person[NUM];
-	_path path;
-	_station station[NUM];
-};
-
-struct __person {
-	_status status;
-	int days;
-	int cause;
-	int station;
-};
 
 struct __station {
 	_status worst_affected;
@@ -27,22 +23,30 @@ struct __list {
 	int person_id;
 	struct __list* next;
 };
+
+struct __route {
+	int station_id;
+	struct __route* next_station;
+};
+
+struct __person {
+	_status status;
+	int days;
+	int cause;
+	int station;
+};
+
 struct __path {
 	int person_id;
 	_route* next_station;
 	struct __path* next_person;
 };
-struct __route {
-	int station_id;
-	struct __route* next_station;
+
+struct __day {
+	_person person[NUM];
+	_path path;
+	_station station[NUM];
 };
-typedef struct __day _day;
-typedef struct __person _person;
-typedef struct __station _station;
-typedef struct __path _path;
-typedef struct __route _route;
-typedef struct __list _list;
-typedef enum __status _status;
 
 struct Node  //node for adjacency list
 {
