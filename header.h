f9b8804448f4187eb65b669728dfe086fe7f878b
vayur <vayur.shanbhag@students.iiit.ac.in>
2021-05-05 01:11:18 +0530

    update functions for query 1

--- header.h
+++ header.h
@@ -2,46 +2,49 @@
 #define __HEADER__
 
 #define NUM 100000
-
 int cur_day;
-enum  __status {negative, secondary_contact, primary_contact, positive};
-struct __day {
-	_person person[NUM];
-	_path path;
-	_station station[NUM];
-};
-
-struct __person {
-	_status status;
-	int days;
-	int cause;
-	int station;
-};
-
-struct __station {
-	_status worst_affected;
-	_list* list;
+extern const char* status_mapping[]; 
+//mapping from integer to its corresponding status in string
+struct __route {
+	int station_id;
+	struct __route* next_station;
 };
 struct __list {
 	int person_id;
 	struct __list* next;
 };
+struct __person {
+	int status;
+	int days; // number of days person has been 
+	int cause; // id of the person who could have been cause of infection
+	int station; // id of the station
+};
+
+struct __station {
+	int worst_affected; //indicates the status of the worst affected person on the station
+	struct __list* list; // linked list of all the people on the station
+	float danger_value; 
+};
 struct __path {
 	int person_id;
-	_route* next_station;
-	struct __path* next_person;
+	struct __route* next_station; // pointer to linked list listing the person's route
+	struct __path* next_person; // pointer to the same struct, denoting the path of the next person
 };
-struct __route {
-	int station_id;
-	struct __route* next_station;
-};
+
+struct __day {
+	struct __person person[NUM];
+	struct __path* path;
+	struct __station station[NUM];
+}; // we maintain the status of the all the stations and persons for 15 days
+
+struct __day day[15];
+
 typedef struct __day _day;
 typedef struct __person _person;
 typedef struct __station _station;
 typedef struct __path _path;
 typedef struct __route _route;
 typedef struct __list _list;
-typedef enum __status _status;
 
 struct Node  //node for adjacency list
 {
@@ -61,4 +64,22 @@ struct Graph* create_Graph(int N);//Creates a graph of N vertices
 void addEdge(struct Graph*, int s, int d , int w);//Adds an edge of length w between s and d
 void print_Graph(struct Graph*);// prints the Graph, for testing 
 
+/***** IMPLEMENTED ******/
+/* present in update-functions.c */
+void UpdateData(int id_person, int id_station, int daye);
+void UpdateStation(int id_person, int id_station, int daye);
+void UpdatePerson(int id_person, int id_station, int daye);
+void UpdateRoute(_route* R, int id_person, int daye);
+void RemovePerson(int id_person, int station_from, int daye);
+void AddPerson(int id_person, int station_to, int daye);
+void UpdateForDay(_path*P, int daye);
+void UpdateForPerson(_route* L, int id_person, int daye);
+void Backtrace(int start_day, int end_day);
+
+/* present in general-functions.c */
+float getDangerIndex(int id_person, int daye);
+int getWorstAffected(int id_station, int daye);
+_list* DeleteNodeById(_list* L, int id_person);
+_list* AddNodeAtStart(_list* L, int id_person);
+void PrintStatus(int n);
 #endif
---
+++ update-functions.c
@@ -0,0 +1,129 @@
+#include "header.h"
+#include <stdlib.h>
+#include <stdio.h>
+void UpdateData(int id_person, int id_station, int daye)
+{
+	if(day[daye].person[id_person].status >= 2 || day[daye].station[id_station].worst_affected >= 2) {
+		if(day[daye].person[id_person].status > day[daye].station[id_station].worst_affected) {
+			UpdateStation(id_person, id_station, daye);
+		}
+		// ---> person coming to the station is more infected than whole station.
+		else if (day[daye].person[id_person].status < day[daye].station[id_station].worst_affected) {
+			UpdatePerson(id_person, id_station, daye);
+		}
+		// ---> person coming to the station is less infected than whole station.
+		// If their status are same, then do nothing
+	}
+}
+void UpdateStation(int id_person, int id_station, int daye)
+{
+	_list* L;
+	L = day[daye].station[id_station].list;
+	while(L != NULL) {
+		day[daye].person[L->person_id].status = day[daye].person[id_person].status -1;
+		day[daye].person[L->person_id].cause = id_person;
+		day[daye].person[L->person_id].days = 0;
+		L = L->next;
+	}
+	// Changed the status of all people on the station.
+	day[daye].station[id_station].worst_affected = day[daye].person[id_person].status -1;
+}
+void UpdatePerson(int id_person, int id_station, int daye)
+{
+	_list* L;
+	day[daye].person[id_person].status = day[daye].station[id_station].worst_affected -1;
+	L = day[daye].station[id_station].list;
+	while(L != NULL) {
+		if(day[daye].person[L->person_id].status == day[daye].station[id_station].worst_affected)
+			break;
+		L = L->next;
+	}
+	//iterating through all the people on the station to find one of the people who are worst affected.
+	//The first such person will be the cause.
+	if(L == NULL) {
+		fprintf(stderr, "ERROR!!, no one with worst affected status");
+		return;
+	}
+	day[daye].person[id_person].cause = L->person_id; // cause will be any arbitrary person on the station who has status same as worst affected.
+	day[daye].person[id_person].days = 0;
+}
+void UpdateRoute(_route* R, int id_person, int daye)
+{
+	while(R != NULL) {
+		UpdateData(id_person, R->station_id, daye);
+		R = R->next_station;
+	}
+}
+void RemovePerson(int id_person, int station_from, int daye)
+{
+	// first check if person is at station_from, and remove it from the station linked list.
+	
+	_list* L;
+	L = day[daye].station[station_from].list;
+	L = DeleteNodeById(L,id_person);
+	day[daye].station[station_from].list = L;
+
+	// then update the station's danger value.
+	day[daye].station[station_from].danger_value -= getDangerIndex(id_person,daye);
+	
+	// check if person moved was worst affected, in that case, update worst affected status
+	if(day[daye].person[id_person].status == day[daye].station[station_from].worst_affected)
+		day[daye].station[station_from].worst_affected = getWorstAffected(station_from, daye);
+}
+void AddPerson(int id_person, int station_to, int daye)
+{
+	// Therefore we have to update the danger value, worst affected and id_person to the list.
+	
+	_list* L = day[daye].station[station_to].list;
+	L = AddNodeAtStart(L,id_person);
+	day[daye].station[station_to].list = L; 
+	day[daye].station[station_to].danger_value += getDangerIndex(id_person,daye);
+	if(day[daye].person[id_person].status > day[daye].station[station_to].worst_affected)
+		day[daye].station[station_to].worst_affected += 1;
+	
+
+	// update the location of the person:
+	day[daye].person[id_person].station = station_to;
+}
+/* The functions above are to be used for mainly two purposes.
+ * We have to 
+	* 1. Update the status everyday a person travels from one place to another.
+	* 2. BackTrace and update the existing data based on new information given in the form of query 1.
+* for the first type of update, we have to work route by route.
+* given a route, we should first remove the person from that particular station, update the station variable.
+* then, visit all the stations given by the route linked list and update the status of the person or the stations he visited accordingly.
+* finally we have to add the person to the destination station. this is done by updating the linked list for the station struct and also the location of the person.
+*
+*
+*
+* for the second type of update, we have to work day by day.
+* given that a list of people were covid positive since a particular day, we must look at all the travels made by peole on each day.
+* for one day, we have to traverse the path linked list of linked lists and and update the values accordingly and print the results
+*/
+
+void UpdateForPerson(_route* L, int id_person, int daye)
+{
+	int station_from = L->station_id;
+	int station_to; 
+	RemovePerson(id_person, station_from, daye);
+	UpdateRoute(L->next_station,id_person,daye);
+	while(L != NULL) {
+		station_to = L->station_id;
+		L = L->next_station;
+	}
+	AddPerson(id_person, station_to, daye);
+}
+void UpdateForDay(_path* P, int daye) {
+	while(P != NULL) {
+		UpdateRoute(P->next_station, P->person_id, daye);
+		P = P->next_person;
+	}
+}
+void Backtrace(int start_day, int end_day)
+{
+	_path* P;
+	for(int i = start_day; i <= end_day; i++) {
+		P = day[i].path;
+		UpdateForDay(P,i);
+	}
+}
